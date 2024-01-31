c1f24601bf32646ade8702e15045d9ab15dbac89
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2024-01-31 22:54:27 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.05 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.swift
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.swift
@@ -1,13 +1,3 @@
 func solution(_ n:Int64) -> [Int] {
-    
-    var count = String(n).count
-    
-    var numArray = Array(repeating: 0, count: count)
-    
-    for i in String(n) {
-        count -= 1
-        numArray[count] = Int(String(i))!
-    }
-    
-    return numArray
+    return String(n).map{Int(String($0))!}.reversed()
 }
