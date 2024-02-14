75fc3f874304fffceaacfc1080a8e701b5fca05c
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2024-02-14 22:47:25 +0900

    [Bronze V] Title: 시험 성적, Time: 8 ms, Memory: 62224 KB -BaekjoonHub

---
+++ 백준/Bronze V/9498. 시험 성적/시험 성적.swift
@@ -0,0 +1,12 @@
+let input = Int(readLine()!)!
+if input > 89 {
+    print("A")
+} else if input > 79 {
+    print("B")
+} else if input > 69 {
+    print("C")
+} else if input > 59 {
+    print("D")
+} else {
+    print("F")
+}
