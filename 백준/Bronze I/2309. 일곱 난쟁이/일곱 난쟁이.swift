803c00abb9e768cf975c76a4ff2339f54ee8c8b8
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2024-02-14 22:43:15 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze I/2309. 일곱 난쟁이/일곱 난쟁이.swift
@@ -0,0 +1,27 @@
+var arr = [Int]()
+
+for _ in 0..<9 {
+    arr.append(Int(readLine()!)!)
+}
+
+var sum = arr.reduce(0, +)
+
+for i in 0..<arr.count - 1 {
+    for j in i+1..<arr.count {
+        if sum - arr[i] - arr[j] == 100 {
+            arr.remove(at:i)
+            arr.remove(at:j-1)
+            
+//            arr.sorted()
+//                .forEach{print($0)}
+            break
+        }
+    }
+    if arr.count == 7 {
+        break
+    }
+}
+
+for i in arr.sorted() {
+    print(i)
+}
