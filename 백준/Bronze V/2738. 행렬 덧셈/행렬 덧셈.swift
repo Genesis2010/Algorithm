b53eb8fbd1c9a024432f1b2cb29603efe597358f
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2024-02-14 22:44:40 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 20 ms, Memory: 69240 KB -BaekjoonHub

---
+++ 백준/Bronze V/2738. 행렬 덧셈/행렬 덧셈.swift
@@ -0,0 +1,27 @@
+let input = readLine()!.split(separator: " ").map{ Int($0)! }
+
+let n = input[0]
+let m = input[1]
+
+var matrix1: [[Int]] = []
+var matrix2: [[Int]] = []
+var result = Array(repeating: Array(repeating:0, count: m), count: n)
+
+for _ in 0..<n {
+    matrix1.append(readLine()!.split(separator: " ").map{ Int($0)! })
+}
+
+for _ in 0..<n {
+    matrix2.append(readLine()!.split(separator: " ").map{ Int($0)! })
+}
+
+for i in 0..<n {
+  for j in 0..<m {
+      result[i][j] = matrix1[i][j] + matrix2[i][j]
+  }
+}
+
+for i in 0..<n {
+    result[i].forEach { print($0, terminator: " ") }
+    print()
+}
