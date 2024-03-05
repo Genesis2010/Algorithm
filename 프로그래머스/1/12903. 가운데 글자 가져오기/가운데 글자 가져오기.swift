ffa7c8e01937571b0cc9de0283fcc70e52eaf47c
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2024-03-05 21:46:41 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.05 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.swift
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.swift
@@ -2,12 +2,9 @@ func solution(_ s:String) -> String {
     
     var str = s.map { String($0) }
     let middle = Int(str.count / 2)
-    var result = ""
     if s.count % 2 == 0 {
-        result = "\(str[middle - 1])" + "\(str[middle])"
+        return "\(str[middle - 1])" + "\(str[middle])"
     } else {
-        result = str[middle]
+        return str[middle]
     }
-    
-    return result
 }
