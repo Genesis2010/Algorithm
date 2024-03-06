db8f7dffdfb39d0e43e863a6b6a39003e367032b
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2024-03-06 14:16:22 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 13.50 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.swift
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.swift
@@ -1,25 +1,4 @@
 func solution(_ strings:[String], _ n:Int) -> [String] {
     
-    var nStrings = [(String, Int)]()
-    var result = [String]()
-    
-    for (i, str) in strings.enumerated() {
-        let strArray = Array(str)
-        let stringsTuple = (String(strArray[n]), i)
-        nStrings.append(stringsTuple)
-    }
-    
-    let sortNStrings = nStrings.sorted { (first, second) in
-                                            if first.0 == second.0 {
-                                                return strings[first.1] < strings[second.1]
-                                            } else {
-                                                return first.0 < second.0
-                                            }
-                                        }
-    
-    for i in sortNStrings {
-        result.append(strings[i.1])
-    }
-    
-    return result
+    return strings.sorted { Array($0)[n] == Array($1)[n] ? $0 < $1 :  Array($0)[n] < Array($1)[n] }
 }
