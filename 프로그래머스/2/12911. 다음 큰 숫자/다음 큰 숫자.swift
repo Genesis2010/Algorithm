e5f31e29fe1ed585bf331f7b6a22410f1590c34c
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2024-03-07 15:31:22 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.00 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.swift
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.swift
@@ -3,15 +3,13 @@ import Foundation
 func solution(_ n:Int) -> Int
 {
     
-    let binaryN = String(n, radix: 2)
-    let nCount1 = binaryN.filter { $0 == "1" }.count
-    var nextNum = n
-    var nextCount1 = 0
+    var nextN = n + 1
     
-    while nCount1 != nextCount1 {
-        nextNum += 1
-        nextCount1 = String(nextNum, radix: 2).filter { $0 == "1" }.count
+    while true {
+    	if n.nonzeroBitCount == nextN.nonzeroBitCount {
+        	break;
+        }
+        nextN += 1
     }
-    
-    return nextNum
+    return nextN
 }
