6a926ea4158429a8a945cf64be3e0d4c0d6aef4c
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2024-03-11 01:21:02 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.61 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.swift
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.swift
@@ -2,7 +2,6 @@ import Foundation
 
 func solution(_ n:Int, _ words:[String]) -> [Int] {
     
-    var result = [0,0]
     var tempStr = words[0]
     var temp = [words[0]]
 
@@ -10,21 +9,15 @@ func solution(_ n:Int, _ words:[String]) -> [Int] {
         if !temp.contains(words[i]) {
             temp.append(words[i])
         } else {
-            result.removeAll()
-            result.append((i % n) + 1)
-            result.append(Int(i / n) + 1)
-            break
+            return [(i % n) + 1, Int(i / n) + 1]
         }
         
         if !(words[i].first == tempStr.last) {
-            result.removeAll()
-            result.append((i % n) + 1)
-            result.append(Int(i / n) + 1)
-            break
+            return [(i % n) + 1, Int(i / n) + 1]
         } else {
             tempStr = words[i]
         }
     }
     
-    return result
+    return [0,0]
 }
