198b67f1999f028c09c954c2ae873d0b64c4ec35
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2024-03-10 13:42:13 +0900

    [level 2] Title: 짝지어 제거하기, Time: 265.76 ms, Memory: 40.2 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.swift
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.swift
@@ -1,11 +1,20 @@
+import Foundation
+
 func solution(_ s:String) -> Int {
     
     var stack = [String]()
     var str = s.map{String($0)}
     
     for i in 0..<s.count {
-        if !stack.isEmpty && stack.last == str[i] {
-            stack.popLast()
+        if !stack.isEmpty {
+            let letter = stack.popLast()!
+            
+            if letter == str[i] {
+                continue
+            } else {
+                stack.append(letter)
+                stack.append(str[i])
+            }
         } else {
             stack.append(str[i])
         }
