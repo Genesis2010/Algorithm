4998ad1402112abe67152ecbb12bae813465e9e2
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2024-03-27 15:45:08 +0900

    [Gold IV] Title: 부분합, Time: 44 ms, Memory: 76292 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.swift
+++ 백준/Gold/1806. 부분합/부분합.swift
@@ -1,23 +1,23 @@
-import Foundation
-let ns = readLine()!.split(separator: " ").map{Int(String($0))!}
-let n = ns[0]
-let s = ns[1]
-var arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let n = input[0], s = input[1]
+let array = readLine()!.split(separator: " ").map { Int($0)! }
 
+var count = n+1
+var start = 0
 var end = 0
-var sum = 0
-var count = 0
-var result = Int.max
-for start in 0..<n{
-    while sum < s && end < n{
-        sum += arr[end]
+var sum = 0 
+
+while end <= n {
+    if sum >= s {
+        count = min(count, end - start)
+        sum -= array[start]
+        start += 1
+    } else if end < n {
+        sum += array[end]
         end += 1
-        count += 1
+    } else {
+        break
     }
-    if sum >= s{
-        result = min(result, count)
-    }
-    sum -= arr[start]
-    count -= 1
 }
-print(result == Int.max ? 0 : result)
+
+print(count == n+1 ? 0 : count)
