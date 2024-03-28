b3c5acf73170faba7cb84476d83d5222cd93323e
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2024-03-28 11:54:13 +0900

    [level 1] Title: 최소직사각형, Time: 3.14 ms, Memory: 19.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.swift
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.swift
@@ -1,21 +1,13 @@
 import Foundation
 
 func solution(_ sizes:[[Int]]) -> Int {
-    var size = sizes
-    let count = size.count
-    var x: [Int] = []
-    var y: [Int] = []
-    var xMax = 0
-    var yMax = 0
-
-    for i in 0..<count {
-        size[i].sort()
-        x.append(size[i][0])
-        y.append(size[i][1])
+    var w = [Int]()
+    var h = [Int]()
+    
+    for i in 0..<sizes.count {
+        w.append(max(sizes[i][0], sizes[i][1]))
+        h.append(min(sizes[i][0], sizes[i][1]))
     }
     
-    xMax = x.max()!
-    yMax = y.max()!
-
-    return x.max()! * y.max()!
+    return w.max()! * h.max()!
 }
