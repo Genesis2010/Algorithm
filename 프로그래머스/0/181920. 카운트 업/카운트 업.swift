54059a9ffadc5c6ea07cfcdd12c333ccb0e8224e
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2024-03-30 01:28:33 +0900

    [level 0] Title: 카운트 업, Time: 0.06 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.swift
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.swift
@@ -1,12 +1,5 @@
 import Foundation
 
 func solution(_ start_num:Int, _ end_num:Int) -> [Int] {
-    
-    var result = [Int]()
-    
-    for i in start_num...end_num {
-        result.append(i)
-    }
-    
-    return result
+    return (start_num...end_num).map { $0 }
 }
