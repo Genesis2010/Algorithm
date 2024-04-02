9d096e8c7893ce5c01b69e08a572ef627908f6c0
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2024-04-03 01:21:54 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.swift
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.swift
@@ -1,10 +1,5 @@
 import Foundation
 
 func solution(_ my_string:String, _ k:Int) -> String {
-    
-    var result = ""
-    for i in 1...k {
-        result += my_string
-    }
-    return result
+    return String(repeating: my_string, count: k)
 }
