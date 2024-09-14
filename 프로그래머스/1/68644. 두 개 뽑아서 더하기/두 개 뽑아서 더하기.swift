f496526679d167ca47e3c047ba5f44283b33e813
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2024-09-15 00:52:42 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 3.82 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ numbers:[Int]) -> [Int] {
+    var result = [Int]()
+    
+    for i in 0..<numbers.count {
+        for j in 0..<numbers.count {
+            if i != j {
+                result.append(numbers[i] + numbers[j])
+            }
+        }
+    }
+
+    return Set(result).sorted(by: <)
+}
