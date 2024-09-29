0139d03d840aa0bc849dc15d3efaedddb1ad1241
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2024-09-29 23:26:13 +0900

    [level 1] Title: 신고 결과 받기, Time: 9328.33 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+func solution(_ id_list:[String], _ report:[String], _ k:Int) -> [Int] {
+    var result = Array(repeating: 0, count: id_list.count)
+    var reportData = [String: Set<String>]()
+    
+    for data in report {
+        let splitData = data.split(separator: " ").map{ String($0) }
+
+        reportData[splitData[1], default: Set()].insert(splitData[0])
+    }
+    
+    for value in reportData.values {
+        if value.count >= k {
+            for person in value {
+                result[id_list.firstIndex(of: person)!] += 1
+            }
+        }
+    }
+    
+    return result
+}
