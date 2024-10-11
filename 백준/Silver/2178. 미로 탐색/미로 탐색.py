d41eca14f81a6f5fa0f2b2111a3c1806acc26189
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2024-10-11 21:57:40 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34088 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,39 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+visited = []
+
+def bfs(n, m, MAP):
+    global visited
+    visited = [[0] * m for _ in range(n)]
+    queue = deque([(0, 0)])
+    visited[0][0] = 1
+
+    while queue:
+        y, x = queue.popleft()
+        
+        if y == n-1 and x == m-1:
+            break
+
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+
+            # 유효 범위 안에 있는지 먼저 체크해야 함
+            if 0 <= ny < n and 0 <= nx < m:
+                # 방문하지 않았고, 이동할 수 있는 칸인 경우
+                if visited[ny][nx] == 0 and MAP[ny][nx] == 1:
+                    visited[ny][nx] = visited[y][x] + 1
+                    queue.append((ny, nx))
+
+def solution():
+    N, M = map(int, input().split())
+    MAP = [list(map(int, input().strip())) for _ in range(N)]
+    bfs(N, M, MAP)
+
+    print(visited[N-1][M-1])
+
+solution()
