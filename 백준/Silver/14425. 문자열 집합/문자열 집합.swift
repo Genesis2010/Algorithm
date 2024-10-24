11d01a10911f72060b720eb8028aa12dc47c022d
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2024-10-24 17:41:57 +0900

    [Silver IV] Title: 문자열 집합, Time: 92 ms, Memory: 74956 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.swift
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.swift
@@ -2,20 +2,13 @@ let input = readLine()!.split(separator: " ").map{Int($0)!}
 let (n, m) = (input[0], input[1])
 var result = 0
 var s = Set<String>()
-var arrayM = [String]()
 
 for _ in 0..<n {
     s.insert(readLine()!)
 }
 
 for _ in 0..<m {
-    arrayM.append(readLine()!)
-}
-
-for i in arrayM {
-    if s.contains(i) {
-        result += 1
-    }
+    if s.contains(readLine()!) { result += 1 }
 }
 
 print(result)
