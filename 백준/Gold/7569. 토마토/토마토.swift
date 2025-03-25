8da8bb1250e698de885dc85462da4c98501e8ec6
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2025-03-25 15:12:40 +0900

    [Gold V] Title: 토마토, Time: 336 ms, Memory: 138436 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.swift
+++ 백준/Gold/7569. 토마토/토마토.swift
@@ -1,9 +1,9 @@
+import Foundation
+
 let mnhInput = readLine()!.split(separator: " ").map{Int($0)!}
 let m = mnhInput[0], n = mnhInput[1], h = mnhInput[2] // m 열, n 행, h 높이
 var box = Array(repeating: Array(repeating: Array(repeating: -1, count: m),count: n),count: h)
-var visited = Array(repeating: Array(repeating: Array(repeating: false, count: m),count: n),count: h)
-var result = 0
-var is0 = [Bool]()
+var queue = [(Int, Int, Int)]()
 
 let dx = [0, 0, -1, 1, 0, 0]
 let dy = [0, 0, 0, 0, -1, 1]
@@ -11,59 +11,48 @@ let dz = [1, -1, 0, 0, 0, 0]
 
 for z in 0..<h {
     for x in 0..<n {
-        let input = readLine()!.split(separator: " ").map{Int($0)!}
-        is0.append(input.contains(0))
-        box[z][x] = input
+        box[z][x] = readLine()!.split(separator: " ").map{Int($0)!}
+        
+        for y in 0..<m {
+            if box[z][x][y] == 1 {
+                queue.append((z,x,y))
+            }
+        }
     }
 }
 
-if is0.filter({ $0 == true }).count == 0 {
-    print(0)
-} else {
-    var index = 0
-    var resultDay = 0
-    var queue = findIndices(target: 1, array: box)
+var index = 0
 
-    while index < queue.count {
-        let (z, y, x, day) = queue[index]
-        visited[z][y][x] = true
-        resultDay = max(resultDay, day)
+while index < queue.count {
+    let (z, y, x) = queue[index]
+    
+    for i in 0..<6 {
+        let nx = x + dx[i], ny = y + dy[i], nz = z + dz[i]
         
-        for i in 0..<6 {
-            let nx = x + dx[i], ny = y + dy[i], nz = z + dz[i]
+        if nx >= 0 && nx < m && ny >= 0 && ny < n && nz < h && nz >= 0 {
+            if box[nz][ny][nx] == 0 {
+                box[nz][ny][nx] = box[z][y][x] + 1
+                queue.append((nz, ny, nx))
+            }
+        }
+    }
+    
+    index += 1
+}
+
+var maxDay = 0
+
+for z in 0..<h {
+    for y in 0..<n {
+        for x in 0..<m {
+            if box[z][y][x] == 0 {
+                print(-1)
+                exit(0)
+            }
             
-            if nx >= 0 && nx < box[0][0].count && ny >= 0 && ny < box[0].count && nz < box.count && nz >= 0 {
-                if box[nz][ny][nx] == 0 && !visited[nz][ny][nx] {
-                    visited[nz][ny][nx] = true
-                    box[nz][ny][nx] = 1
-                    queue.append((nz, ny, nx, day + 1))
-                }
-            }
+            maxDay = max(maxDay, box[z][y][x])
         }
-        
-        index += 1
-    }
-    
-    if findIndices(target: 0, array: box).count > 0 {
-        print(-1)
-    } else {
-        print(resultDay)
     }
 }
 
-
-func findIndices(target: Int, array: [[[Int]]]) -> [(Int, Int, Int, Int)] {
-    var result: [(Int, Int, Int, Int)] = []
-    
-    for i in 0..<array.count {
-        for j in 0..<array[i].count {
-            for k in 0..<array[i][j].count {
-                if array[i][j][k] == target {
-                    result.append((i, j, k, 0))
-                }
-            }
-        }
-    }
-    
-    return result
-}
+print(maxDay - 1)
