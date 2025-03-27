413171127f69a4cf7382a38a1c46ba3529fa846e
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2025-03-27 17:05:54 +0900

    [Silver V] Title: 숫자 카드, Time: 512 ms, Memory: 117964 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.swift
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.swift
@@ -1,39 +1,8 @@
 let n = Int(readLine()!)!
-let nArray = readLine()!.split(separator: " ").map{Int($0)!}
-let nArraySort = nArray.sorted()
+let nArray = Set(readLine()!.split(separator: " ").map{Int($0)!})
 
 let m = Int(readLine()!)!
 let mArray = readLine()!.split(separator: " ").map{Int($0)!}
-var result = Array(repeating: 0, count: m)
 
-var mIndexDic = [Int: Int]()
-
-for i in 0..<mArray.count {
-    mIndexDic[mArray[i]] = i
-}
-
-var containsMArray = [Int]()
-let mArraySort = mArray.sorted()
-
-var nIndex = 0
-var mIndex = 0
-
-while nIndex < n && mIndex < m {
-    if nArraySort[nIndex] < mArraySort[mIndex] {
-        nIndex += 1
-    } else if nArraySort[nIndex] > mArraySort[mIndex]{
-        mIndex += 1
-    } else {
-        containsMArray.append(mArraySort[mIndex])
-        nIndex += 1
-        mIndex += 1
-    }
-}
-
-for i in containsMArray {
-    result[mIndexDic[i]!] = 1
-}
-
-for i in result {
-    print(i, terminator: " ")
-}
+var result = mArray.map{ nArray.contains($0) ? "1" : "0" }
+print(result.joined(separator: " "))
