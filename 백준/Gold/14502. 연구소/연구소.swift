6696df416cca98289a1a78c2fb19958263100f3e
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2025-03-27 10:21:27 +0900

    [Gold IV] Title: 연구소, Time: 112 ms, Memory: 69108 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.swift
@@ -0,0 +1,81 @@
+let nm = readLine()!.split(separator: " ").map{Int($0)!}
+let n = nm[0], m = nm[1]
+
+var map = [[Int]](repeating: [], count: n)
+
+var emptyPositions = [(Int, Int)]()
+var virusPositions = [(Int, Int)]()
+
+var maxSafeArea = 0
+
+for i in 0..<n {
+    map[i] = readLine()!.split(separator: " ").map{ Int($0)! }
+}
+
+for i in 0..<n {
+    for j in 0..<m {
+        if map[i][j] == 0 {
+            emptyPositions.append((i, j))
+        } else if map[i][j] == 2 {
+            virusPositions.append((i, j))
+        }
+    }
+}
+
+let dx = [-1, 1, 0, 0]
+let dy = [0, 0, -1 ,1]
+
+for i in 0..<emptyPositions.count {
+    for j in i+1..<emptyPositions.count {
+        for k in j+1..<emptyPositions.count {
+            var tempMap = map
+            
+            let position1 = emptyPositions[i]
+            let position2 = emptyPositions[j]
+            let position3 = emptyPositions[k]
+            
+            tempMap[position1.0][position1.1] = 1
+            tempMap[position2.0][position2.1] = 1
+            tempMap[position3.0][position3.1] = 1
+            
+            bfs(tempMap)
+        }
+    }
+}
+
+print(maxSafeArea)
+
+func bfs(_ tempMap: [[Int]]) {
+    var tempMap = tempMap
+    var index = 0
+    var queue = virusPositions
+    
+    while index < queue.count {
+        let (x, y) = queue[index]
+        
+        for i in 0..<4 {
+            let nx = x + dx[i]
+            let ny = y + dy[i]
+            
+            if nx >= 0, nx < n, ny >= 0, ny < m, tempMap[nx][ny] == 0 {
+                tempMap[nx][ny] = 2
+                queue.append((nx, ny))
+            }
+        }
+        
+        index += 1
+    }
+    
+    var temp = 0
+     for i in 0..<n {
+         for j in tempMap[i] {
+             if j == 0 {
+                 temp += 1
+             }
+         }
+     }
+    
+     if maxSafeArea < temp {
+         maxSafeArea = temp
+     }
+}
